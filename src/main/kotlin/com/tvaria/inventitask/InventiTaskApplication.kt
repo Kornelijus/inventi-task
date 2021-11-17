c456c86d538dc8bd7f10baababbbbad4b260dce1
Kornelijus Tvarijanavičius <kornelijus@tvaria.com>
2021-11-17 18:02:35 +0200

    Init Project with IntelliJ Spring Initializr

---
+++ src/main/kotlin/com/tvaria/inventitask/InventiTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.tvaria.inventitask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventiTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<InventiTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/tvaria/inventitask/InventiTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tvaria.inventitask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InventiTaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
