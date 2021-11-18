a99025e7062b13e91f67230f95c021764e8f4c93
Kornelijus Tvarijanavičius <kornelijus@tvaria.com>
2021-11-18 22:22:07 +0200

    Refactor long class names

--- src/main/kotlin/com/tvaria/inventitask/TaskApplication.kt
+++ src/main/kotlin/com/tvaria/inventitask/TaskApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class InventiTaskApplication
+class TaskApplication
 
 fun main(args: Array<String>) {
-    runApplication<InventiTaskApplication>(*args)
+    runApplication<TaskApplication>(*args)
 }
--- src/test/kotlin/com/tvaria/inventitask/TaskApplicationTests.kt
+++ src/test/kotlin/com/tvaria/inventitask/TaskApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class InventiTaskApplicationTests {
+class TaskApplicationTests {
 
     @Test
     fun contextLoads() {
