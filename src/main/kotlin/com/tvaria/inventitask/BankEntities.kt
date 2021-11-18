55364244cfbb554334ec7bce00df3ea3e942b55b
Kornelijus Tvarijanavičius <kornelijus@tvaria.com>
2021-11-18 23:41:09 +0200

    Add OpenCSV Annotations to BankTransaction Entity

--- src/main/kotlin/com/tvaria/inventitask/BankEntities.kt
+++ src/main/kotlin/com/tvaria/inventitask/BankEntities.kt
@@ -1,5 +1,9 @@
 package com.tvaria.inventitask
 
+import com.opencsv.bean.CsvBindByName
+import com.opencsv.bean.CsvBindByPosition
+import com.opencsv.bean.CsvDate
+import com.opencsv.bean.CsvIgnore
 import org.hibernate.annotations.NaturalId
 import java.math.BigDecimal
 import java.time.LocalDateTime
@@ -16,13 +20,31 @@ class BankAccount(
 
 @Entity(name = "Transaction")
 class BankTransaction(
+    @CsvBindByName @CsvBindByName(column = "account")
+    @CsvBindByPosition(position = 0)
     var accountNumber: String,
-    // This will be in UTC as the timezone is set in application.properties
-    var operationTime: LocalDateTime,
+
+    @CsvBindByName @CsvBindByName(column = "date") @CsvBindByName(column = "time")
+    @CsvBindByPosition(position = 1)
+    @CsvDate
+    var operationTime: LocalDateTime, // This will be in UTC as the timezone is set in application.properties
+
+    @CsvBindByName
+    @CsvBindByPosition(position = 2)
     var beneficiary: String,
+
+    @CsvBindByName @CsvBindByName(column = "reference")
+    @CsvBindByPosition(position = 3)
     var comment: String? = null,
-    // TODO: make sure the transaction amount is non-negative
-    var amount: BigDecimal,
+
+    @CsvBindByName
+    @CsvBindByPosition(position = 4)
+    var amount: BigDecimal, // TODO: make sure the transaction amount is non-negative
+
+    @CsvBindByName
+    @CsvBindByPosition(position = 5)
     var currency: Currency,
+
+    @CsvIgnore
     @Id @GeneratedValue var id: Long
 )
