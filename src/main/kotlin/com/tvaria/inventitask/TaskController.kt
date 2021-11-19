8c0627fac44491ff8e3d45b25f841af4fc7afe5f
Kornelijus Tvarijanavičius <kornelijus@tvaria.com>
2021-11-19 22:10:21 +0200

    Implement TaskController endpoints

--- src/main/kotlin/com/tvaria/inventitask/TaskController.kt
+++ src/main/kotlin/com/tvaria/inventitask/TaskController.kt
@@ -1,27 +1,33 @@
 package com.tvaria.inventitask
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import java.math.BigDecimal
+import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 @RestController
-class TaskController {
+class TaskController(@Autowired val accountService: AccountService, @Autowired val statementService: StatementService) {
 
-    @PostMapping("/import")
-    fun importCSV(csvFile: MultipartFile, timeZone: String = "Ect/UTC") {
-        /* TODO: Assuming some banks provide dates in random timezones, not UTC,
-            which needs to be handled before writing as UTC to the database. */
-        TODO("Import bank statement for one or several bank accounts via CSV")
+    // Specifying multipart/form-data as the content type fixes the issue of Swagger showing @RequestPart only as json,
+    // not sure if there's a better way to solve this or if it's just a bug with springdoc-openapi
+    @PostMapping("/import", consumes = ["multipart/form-data"])
+    fun importCSV(@RequestPart csvFile: MultipartFile) {
+        statementService.importStatement(csvFile.inputStream)
+        // TODO: return a meaningful response
     }
 
-    @GetMapping("/export")
-    fun exportCSV(from: String?, to: String?) {
-        TODO("Export bank statement for one or several bank accounts via CSV")
+    @GetMapping("/export", produces = ["text/csv"])
+    fun exportCSV(response: HttpServletResponse, from: String?, to: String?) {
+        statementService.exportStatement(response, from, to)
     }
 
     @GetMapping("/balance")
-    fun calculateBalance(accountNumber: String, from: String?, to: String?) {
-        TODO("Calculate account balance for given date")
+    fun calculateBalance(accountNumber: String, from: String?, to: String?): HashMap<Currency, BigDecimal> {
+        return accountService.getAccountBalance(accountNumber, from, to)
     }
 }
